66e88abcfc49a3cdce1ffa576da56d72b735051d
Steve Sutherland <sbsuth@comcast.net>
2018-07-17 21:21:18 -0700

    Add params

--- hass_test.js
+++ hass_test.js
@@ -1,12 +1,26 @@
 var argv = process.argv;
 var cmd = argv[2];
+var unit = argv[3];
 const WebSocket = require('ws');
 global.WebSocket = WebSocket;
 const HAWS = require("home-assistant-js-websocket");
 
 const getWsUrl = haUrl => `ws://${haUrl}/api/websocket`;
-var name="switch.60_cube_dc_power";
-//var name="switch.bathroom_units_dc_power";
+
+if ((cmd != "turn_on") && (cmd != "turn_off")) {
+	console.log("ERROR: Command must be turn_on or turn_off");
+	process.exit();
+}
+
+var name;
+if ((unit == undefined) || (unit == 0)) {
+	name ="switch.60_cube_dc_power";
+} else if (unit == 1) {
+	name="switch.bathroom_units_dc_power";
+} else {
+	console.log("ERROR: Unit must be 0 or 1");
+	process.exit();
+}
 
 var url = 'localhost:8123';
 console.log("Creating connection");
